59b49e2a9f6b77b8825ef6d8075e91d5a9aea16a
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-04-02 00:05:45 +0530

    setup prettier

---
+++ src/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "brand-name" }, "Dog Pawri"),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
