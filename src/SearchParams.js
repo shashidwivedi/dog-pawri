88951d6d42182464c93b4516190d69ac6485a72a
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-04-04 20:08:50 +0530

    add breed select menu in SearchParams component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -5,6 +5,8 @@ const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -29,6 +31,21 @@ const SearchParams = () => {
           {ANIMALS.map((animal) => (
             <option value={animal} key={animal}>
               {animal}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          name=""
+          id="breed"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+          onBlur={(e) => setBreed(e.target.value)}
+        >
+          <option value=""></option>
+          {breeds.map((breed) => (
+            <option value={breed} key={breed}>
+              {breed}
             </option>
           ))}
         </select>
