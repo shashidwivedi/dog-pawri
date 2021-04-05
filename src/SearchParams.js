817481836edc24cf4d8abfd238454a99ab838977
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-04-05 14:54:30 +0530

    remove console.log

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -18,11 +18,7 @@ const SearchParams = () => {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
-
     const json = await res.json();
-
-    console.log(json);
-
     setPets(json.pets);
   }
 
