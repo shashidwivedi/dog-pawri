c1734d5722fe2d3f806f5aa8451a47769aac5ff6
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-04-05 13:19:02 +0530

    add useEffect (to request pet data from api)  and Pet Component in SearchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    console.log(json);
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -51,6 +69,14 @@ const SearchParams = () => {
         </select>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
