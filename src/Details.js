42439e7be885d60bd5c01f59c2cb7ca715ab7b0d
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-04-09 01:21:12 +0530

    make Details class component with lifecycle methods and use withRouter HOC

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,41 @@
-const Details = () => {
-  return <h2>Hi!</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    console.log(this.state);
+
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { name, animal, breed, city, state, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
