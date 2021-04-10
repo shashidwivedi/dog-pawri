48ac7b18526b629409b183b0cc7fca025c0a9117
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-04-10 23:58:32 +0530

    babel presets and plugins for es6

--- src/Details.js
+++ src/Details.js
@@ -2,11 +2,7 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
